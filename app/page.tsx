7247ee27f2022bbd7392579bab43e2c3f513b306
shegspear <shegspear@gmail.com>
2024-09-28 12:22:03 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,142 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [score, setScore] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+  const [data, setData] = useState([
+    {
+      id: 1,
+      question: "What is the capital territory of Nigeria ?",
+      options: [
+        { id: "1x", selected: false, value: "Lagos"},
+        {id: "2x", selected: false, value: "Federal Capital Territory"},
+        { id: "3x", selected: false, value: "Port Harcourt"}
+      ],
+      correctOption: "Federal Capital Territory"
+    },
+    {
+      id: 2,
+      question: "What is Lagos state population ?",
+      options: [
+        {id: "1x", selected: false, value: "10 Million"},
+        {id: "2x", selected: false, value: "5 Million"},
+        {id: "3x", selected: false, value: "35 Million"}
+      ],
+      correctOption: "35 Million"
+    },
+    {
+      id: 3,
+      question: "Will Serenity be achieved ?",
+      options: [
+        { id: "1x",selected: false, value: "Yes"},
+        {id: "2x", selected: false, value: "No"}
+      ],
+      correctOption: "No"
+    },
+  ]);
+
+  const checkAnswer = (
+    selectedOpt:string,
+    correctAnswer:string,
+    questionId:number,
+    optionIndex:number
+  ) => {
+    // console.log('selected idx ', optionIndex)
+    // data.forEach((cur:any) => {
+    //   if(cur.id === questionId) {
+    //     cur.options.forEach((opt:any, idx:number) => {
+    //       opt.selected = idx === optionIndex ? true : false
+    //     })
+    //   }
+    // });
+    
+    let arr = data;
+
+    arr.forEach((cur:any) => {
+        if(cur.id === questionId) {
+          cur.options.forEach((opt:any, idx:number) => {
+            opt.selected = opt.id === optionIndex ? true : false;
+            console.log(`${opt.value} ${opt.selected}`)
+          })
+        }
+    });
+
+    setData(arr)
+
+    if(selectedOpt === correctAnswer) {
+      setScore((prev) => prev + 1)
+    } else {
+      setScore((prev) => prev === 0 ? 0 : prev -1)
+    }
+  }
+
+  const move = (forward:boolean) => {
+    if(forward) {
+      setCurrentQuestion((prev) => prev >= data.length ? prev - 1 : prev + 1);
+    } else {
+      setCurrentQuestion((prev) => prev === 0 ? prev : prev - 1);
+    }
+  }
+
+  return (
+    <div className="flex flex-col h-screen justify-center items-center">
+
+      <h1
+        className="font-bold text-3xl mb-8"
+      >
+        Your score : {score}
+      </h1>
+
+      <div
+        className="flex flex-col justify-center items-center border-2 border-slate-500 rounded-xl p-4 w-8/12"
+      >
+        <h3 className="mb-4 font-medium text-2xl">
+          {data[currentQuestion].question}
+        </h3>
+
+       <div
+        className="w-full px-6 mb-4"
+       >
+         {
+            data[currentQuestion].options.map((cur:any, idx:number) => (
+              <div className="mb-2 flex flex-row items-center">
+                <input
+                  className="cursor-pointer rounded-xl h-[18px] w-[18px]" 
+                  type={"radio"} 
+                  id={cur.value} 
+                  checked={cur.selected} 
+                  value={cur.value} 
+                  onChange={() => checkAnswer(cur.value, data[currentQuestion].correctOption, data[currentQuestion].id, cur.id)}
+                />
+                <label className="cursor-pointer ml-2 text-lg font-normal" htmlFor={cur.value}>{cur.value}</label>
+              </div>
+            ))
+          }
+       </div>
+
+       <div
+        className="px-6 w-full flex flex-row justify-between items-center"
+       >
+        <button 
+          onClick={() => move(false)}
+          className="border-red-500 font-bold text-sm"
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+            prev
+        </button>
+
+        <button
+          onClick={() => move(true)}
+          className="font-bold text-sm"
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            next
+        </button>
+       </div>
+
+      </div>
+
     </div>
   );
 }
